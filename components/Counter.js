5dc6c64c9920df478c019f36771429f5274c2917
yogesh193001 <yogeshwargajadur@outlook.com>
2023-11-01 11:06:29 +0400

    ProjectRN1

---
+++ components/Counter.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  render() {
+    return (
+      <View>
+        <Text>C</Text>
+      </View>
+    );
+  }
+}
